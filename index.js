deb2f8db3f422c6256025c2ade6ca4b5f79f0a92
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-12-09 11:09:28 -0300

    feat(teste): teste deploy

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
-
 const app = express();
+const path = require('path');
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
 })
 
 app.listen(process.env.PORT || 3000);
 
-module.exports = app
+module.exports = app;
