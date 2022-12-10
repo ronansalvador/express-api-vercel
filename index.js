8bce1f48f79079d522929c056513d9aa867dbc06
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-12-09 23:03:49 -0300

    feat(route test return json): teste

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+const PATH = './talker.json';
 
 app.use(express.static('public'))
 app.use(bodyParser.json());
@@ -10,8 +12,9 @@ app.get('/', (req, res) => {
     res.sendFile('index.html', {root: path.join(__dirname, 'public')});
 })
 
-app.get('/test', (req, res) => {
-  res.status(200).json({message: 'teste de retorno de API'})
+app.get('/test', async (req, res) => {
+  const talkers = await fs.readFile(PATH, 'utf-8');
+  res.status(200).json(JSON.parse(talkers))
 })
 
 app.listen(process.env.PORT || 3000);
